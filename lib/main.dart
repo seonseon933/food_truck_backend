2df5437a4e079546bdb7256d0fa2e2f66bd6d891
anon <codeparadoxist@gmail.com>
2024-05-02 23:53:18 +0900

    네이버 api 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:food_truck/view/first_view.dart';
+import 'package:food_truck/api/naver_map_api.dart';
 
-void main() => runApp(const MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapApp.init();
+  runApp(const NaverMapApp());
+}
